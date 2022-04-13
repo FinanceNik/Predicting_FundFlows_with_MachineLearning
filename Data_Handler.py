dd700fe0fc07118cf1683186e243783c6cf818e4
FinanceNik <niklas.clasen99@gmail.com>
2022-04-13 19:29:05 +0200

    Created the first function for dataframe manipulation

--- Data_Handler.py
+++ Data_Handler.py
@@ -1,4 +1,27 @@
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot
-import openpyxl
+from matplotlib import pyplot as plt
+pd.set_option('display.max_columns', 500)
+
+
+def data_cleaning():
+    df = pd.read_csv('data/MutualFund prices - A-E.csv')
+    df = df.loc[(df['fund_symbol'] == 'AAAAX')]
+    df.insert(3, 'period_change_value', '')
+    df['period_change_value'] = df['nav_per_share'].pct_change()
+    df.insert(4, 'period_change_positive', '')
+
+    def period_change_calculator(x):
+        if x >= 0.0:
+            return 1
+        else:
+            return 0
+
+    df['period_change_positive'] = df['period_change_value'].apply(period_change_calculator)
+    df.insert(5, 'net_fund_flow', '')
+    df['net_fund_flow'] = df['nav_per_share'].diff(periods=1)
+
+    return df
+
+
+data_cleaning()
---
+++ Neural_Net.py
@@ -0,0 +1,40 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from matplotlib import pyplot as plt
+import pandas as pd
+from tensorflow.keras.callbacks import EarlyStopping
+import seaborn as sns
+import Data_Handler as dh
+
+df = dh.df
+X = df.drop(['Darlehen - Vertragsstatus'], axis=1).values
+y = df['Darlehen - Vertragsstatus'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=101)
+
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = Sequential()
+model.add(Dense(26, activation='relu'))
+model.add(Dense(13, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam')
+model.fit(x=X_train, y=y_train, epochs=10, validation_data=(X_test, y_test))
+
+predictions = model.predict(X_test)
+
+print(confusion_matrix(y_test, predictions))
+print('\n')
+print(classification_report(y_test, predictions))
+
+# losses = pd.DataFrame(model.history.history)
+# losses.plot()
+# plt.show()
+
+
