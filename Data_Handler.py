238d928b90c90fb124f568e20db926ce6238701a
FinanceNik <niklas.clasen99@gmail.com>
2022-04-16 22:56:25 +0200

    Imported the - hopefully - final dataset of off Morningstar. Created a function to concatinate the csv files as only 5000 rows at a time could be exportet. Furthermore working on the first function for exploratory data analysis

--- Data_Handler.py
+++ Data_Handler.py
@@ -1,7 +1,40 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+import seaborn as sns
 pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 500)
+
+
+def dataset_combiner():
+    completed_dataset = []
+    path = '/home/thequant/Downloads/'
+    for i in range(1,8):
+        filename = f'version_1.1_part_{i}.csv'
+        df = pd.read_csv(path+filename)
+        completed_dataset.append(df)
+
+    completed_dataset = pd.concat(completed_dataset)
+    completed_dataset.to_csv('Morningstar_data_version_1.1.csv')
+    print(len(completed_dataset.index))
+
+
+def dataset_cleaner():
+    df = pd.read_csv('data/Morningstar_data_version_1.1.csv')
+    df = df.drop(['Unnamed: 0'], axis=1)
+
+    # --> First I have to clean up the columns, 600 is way too much to visualize.
+    # sns.heatmap(df.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+    # plt.show()
+    # print(df.describe())
+
+
+dataset_cleaner()
+
+
+#######################################################################################################
+# --> Functions below are only for testing purposes and do not contribute to the final analysis.
+#######################################################################################################
 
 
 def data_cleaning():
@@ -37,4 +70,4 @@ def data_filtering():
     return df
 
 
-data_filtering()
+# data_filtering()
